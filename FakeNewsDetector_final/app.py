3294d6ea2885bd677c3df44591f5ba5b7a7aec43
Aditya <60684641+0x0elliot@users.noreply.github.com>
2022-05-29 00:57:10 +0530

    Debugged

--- FakeNewsDetector_final/app.py
+++ FakeNewsDetector_final/app.py
@@ -4,10 +4,7 @@ app = Flask(__name__)
 
 @app.route('/', methods =["GET", "POST"])
 def ai():
-    input_user = ""
-    if request.method == "POST":
-        input_user = request.form['input']
-    print(input_user)
+    input_user = request.args.get("input")
     return render_template("index.html")
 
 app.run(debug=True)
